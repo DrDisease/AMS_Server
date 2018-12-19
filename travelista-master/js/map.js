15f466dee2fe485a6ad671926edf5f25c6cbef63
joaogomes18 <j.gomes@ua.pt>
2018-12-19 09:56:27 +0000

    Mapv2

--- travelista-master/js/map.js
+++ travelista-master/js/map.js
@@ -1,7 +1,5 @@
-var map =newL.Map("oMeuMapa", {center: [40.633258,-8.659097],zoom: 15});
-
-var osmUrl = "http://{s}.title.openstreet.org/{z}/{x}/[{y}.png";
-var osmAttrib="Map data  OpenStreetMap contributors"
-var osm =newL.TileLayer(osmUrl, {attribution: osmAttrib});
-
+var map = new L.Map("oMeuMapa", {center: [40.633258,-8.659097],zoom: 15});
+var osmUrl = "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
+var osmAttrib = "Map data OpenStreetMap contributors";
+var osm = new L.TileLayer(osmUrl, {attribution: osmAttrib});
 map.addLayer(osm);
